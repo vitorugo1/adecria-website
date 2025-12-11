c84fdc03acf04ab3e9ad90f8a00226972a87dfb4
vitorugo1 <hvitor79@gmail.com>
2025-12-11 20:32:41 -0300

    ajuste hero

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,16 +1,24 @@
+import clsx from 'clsx';
 import Image from 'next/image';
 
 export function Hero() {
   const p = 'text-white/85 font-normal text-base md:text-xl';
-  const h2 = 'font-semibold <p-top-2></p-top-2> text-2xl md:text-4xl';
+  const h2 = 'font-semibold text-2xl md:text-4xl';
   return (
     <>
-      <section className='relative w-full flex items-center justify-center'>
+      <section className='relative w-full min-h-screen h-auto flex flex-col items-center justify-center text-center pb-32'>
+        <Image
+          src='/images/hero-mobile.png'
+          alt='Mão de um adulto segurando a de uma criança'
+          fill
+          className='object-cover z-10 md:hidden'
+          priority
+        />
         <Image
           src='/images/hero-banner-img.png'
           alt='Mão de um adulto segurando a de uma criança'
           fill
-          className='object-cover z-10 '
+          className='object-cover z-10 hidden md:block'
           priority
         />
         <div className='z-10 font-bold m-4 text-white p-4 text-3xl md:text-5xl text-center flex flex-col gap-8 font-montserrat'>
@@ -24,10 +32,22 @@ export function Hero() {
             jurídica e psicológica gratuita às famílias.
           </p>
 
-          <div className='w-full h-0 border-t border-white/85 my-6 max-w-xl flex justify-center'>
+          <div
+            className={clsx('w-full max-w-xs border-t border-white/50')}
+          ></div>
+
+          <div className='w-full h-0 max-w-xl flex justify-center flex-wrap gap-8'>
             <div>
               <h2 className={`${h2}`}>5+ anos</h2>
               <p className={`${p} font-sm`}>de atuação</p>
+            </div>
+            <div>
+              <h2 className={`${h2}`}>Centenas</h2>
+              <p className={`${p} font-sm`}>de famílias atendidas</p>
+            </div>
+            <div>
+              <h2 className={`${h2}`}>80-90%</h2>
+              <p className={`${p} font-sm`}>acordos em mediação</p>
             </div>
           </div>
         </div>
